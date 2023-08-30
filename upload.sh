7f013d6e22c41717fa5983e853012246b9137587
laeth <laethenglish@gmail.com>
2023-08-30 09:50:39 -0700

    fix: switched upload.sh to Github CLI

--- upload.sh
+++ upload.sh
@@ -4,12 +4,14 @@
 DEB_PATH="./build/*.deb"
 DEB_FILES=($DEB_PATH)
 
+gh auth login --with-token "$GITHUB_TOKEN"
 for DEB in "${DEB_FILES[@]}"; do
-  UPLOAD_URL=$(curl -s -H "Authorization: token $GITHUB_TOKEN" \
-  "https://api.github.com/repos/PilotGuy772/wikidb/releases/$RELEASE_ID/assets?name=$(basename "$DEB")" | \
-  jq -r '.upload_url' | sed -e 's/{?name,label}//')
-  
-  curl -H "Authorization: token $GITHUB_TOKEN" \
-  -H "Content-type: $(file -b --mime-type $DEB)" \
-  --data-binary @"$DEB" "$UPLOAD_URL"
+#  UPLOAD_URL=$(curl -s -H "Authorization: token $GITHUB_TOKEN" \
+#  "https://api.github.com/repos/PilotGuy772/wikidb/releases/$RELEASE_ID/assets?name=$(basename "$DEB")" | \
+#  jq -r '.upload_url' | sed -e 's/{?name,label}//')
+#  
+#  curl -H "Authorization: token $GITHUB_TOKEN" \
+#  -H "Content-type: $(file -b --mime-type $DEB)" \
+#  --data-binary @"$DEB" "$UPLOAD_URL"
+  gh release upload v1.0.0 "$DEB"
 done
